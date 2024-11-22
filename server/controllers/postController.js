40a0b5865f5ca9bc6d1e9fcfc67cc0f9ee1f9556
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-23 02:11:32 +0530

    Edit and Delete handler code added

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -63,3 +63,73 @@ exports.getPostsByAuthor = async (req, res, next) => {
     next(error);
   }
 };
+
+// Edit a post
+exports.updatePost = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { title, content } = req.body;
+  
+      // Validate input
+      if (!title || !content) {
+        return next(new ErrorHandler('Title and content are required', 400));
+      }
+  
+      // Find and update the post
+      let post = await Post.findById(id);
+      console.log('post to update', post);
+  
+      if (!post) {
+        return next(new ErrorHandler('Post not found', 404));
+      }
+  
+      // Ensure only the author can edit their post
+      if (post.authorId.toString() !== req.user._id.toString()) {
+        return next(new ErrorHandler('You are not authorized to update this post', 403));
+      }
+  
+      post.title = title;
+      post.content = content;
+      post = await post.save();
+      console.log('updated post', post);
+  
+      res.status(200).json({
+        success: true,
+        message: 'Post updated successfully',
+        post
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
+  // Delete a post
+  exports.deletePost = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      // Find the post to delete
+      const post = await Post.findById(id);
+      console.log('post to delete', post);
+  
+      if (!post) {
+        return next(new ErrorHandler('Post not found', 404));
+      }
+  
+      // Ensure only the author can delete their post
+      if (post.authorId.toString() !== req.user._id.toString()) {
+        return next(new ErrorHandler('You are not authorized to delete this post', 403));
+      }
+  
+      await post.deleteOne();
+      console.log('post deleted');
+  
+      res.status(200).json({
+        success: true,
+        message: 'Post deleted successfully'
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createPost, getAllPosts, getPostsByAuthor } = require('../controllers/postController');
+const { createPost, getAllPosts, getPostsByAuthor, updatePost, deletePost } = require('../controllers/postController');
 const { isAuthenticatedUser } = require('../middlewares/auth');
 
 
@@ -9,5 +9,8 @@ router.route('/posts').get(getAllPosts);
 
 // Get posts by a specific author
 router.route('/posts').get(getPostsByAuthor);
+
+router.route('/post/:id').put(isAuthenticatedUser, updatePost);
+router.route('/post/:id').delete(isAuthenticatedUser, deletePost);
 
 module.exports = router;
